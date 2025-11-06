9f0efedf90945d47cb13e1625698ef107cd3c792
Molly Gustafson <mollyannegustaf@gmail.com>
2025-11-05 18:27:40 -0600

    Create goodbyemessage.py

---
+++ goodbyemessage.py
@@ -0,0 +1,27 @@
+# Initialize an empty list named 'suitcase'
+# TODO: Add items to your suitcase
+
+suitcase = []
+suitcase.append("shoes")
+suitcase.append("pants")
+
+# Declare a string variable for your goodbye message
+# TODO: Assign a farewell message to the variable
+
+farewell_message = "Farewell!"
+
+
+# Access the first and last item in your suitcase
+# TODO: Print the first item in the suitcase
+# TODO: Print the last item in the suitcase
+
+first_item = suitcase[0]
+last_item = suitcase[-1]
+
+print(first_item)
+print(last_item)
+
+# Send off with a farewell in uppercase
+# TODO: Print the farewell message in uppercase
+
+print(farewell_message.upper())
